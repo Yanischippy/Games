1409aaa559bf70dc968ceed61d819f7098f084ec
Yanischippy <snorkeling.qwerty@gmail.com>
2026-05-23 18:36:29 +1000

    I added the Speed Potion (Grey) and the 10+ Lives Potion (Green with black 10). I also changed the number of lives from 30 to 50. :D

--- main.ts
+++ main.ts
@@ -15,170 +15,69 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         // Toggle "On"
         arrowtoggle = 1
         // Adding back the left arrow.
-        lparrow = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 1 . . . . . . . . . . . 
-            . . . 1 . . . . . . . . . . . . 
-            . . 1 . . . . . . . . . . . . . 
-            . 1 . . . . . . . . . . . . . . 
-            1 1 1 1 1 1 1 1 . . . . . . . . 
-            . 1 . . . . . . . . . . . . . . 
-            . . 1 . . . . . . . . . . . . . 
-            . . . 1 . . . . . . . . . . . . 
-            . . . . 1 . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Player)
+        lparrow = sprites.create(assets.image`rarrowint`, SpriteKind.Player)
         // Adding back the right arrow.
         rparrow = sprites.create(assets.image`myImage3`, SpriteKind.Player)
     }
 })
+function Potions () {
+    if (ball1.overlapsWith(lifepotion10)) {
+        info.changeLifeBy(10)
+    }
+    if (ball1.overlapsWith(_2xspeedpotion)) {
+        chicken1.setVelocity(30, 140)
+        pizza1.setVelocity(-30, 200)
+        steak1.setVelocity(30, 100)
+        cherrys1.setVelocity(60, 60)
+        apple1.setVelocity(100, 100)
+        strawberry1.setVelocity(0, 300)
+        lifepotion10.setVelocity(0, 50)
+        _2xspeedpotion.setVelocity(0, 50)
+        pause(20000)
+        chicken1.setVelocity(15, 70)
+        pizza1.setVelocity(-15, 100)
+        steak1.setVelocity(15, 50)
+        cherrys1.setVelocity(30, 30)
+        apple1.setVelocity(50, 50)
+        strawberry1.setVelocity(0, 150)
+        lifepotion10.setVelocity(0, 100)
+        _2xspeedpotion.setVelocity(0, 75)
+    }
+}
 info.onLifeZero(function () {
     game.setGameOverScoringType(game.ScoringType.HighScore)
     game.gameOver(false)
 })
 // When turned on.
+let _2xspeedpotion: Sprite = null
+let lifepotion10: Sprite = null
+let strawberry1: Sprite = null
+let apple1: Sprite = null
+let cherrys1: Sprite = null
+let steak1: Sprite = null
+let pizza1: Sprite = null
+let chicken1: Sprite = null
 let rparrow: Sprite = null
 let lparrow: Sprite = null
 let arrowtoggle = 0
+let ball1: Sprite = null
 info.setScore(0)
-let ball1 = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+ball1 = sprites.create(assets.image`myImage1`, SpriteKind.Player)
 controller.moveSprite(ball1)
-info.setLife(30)
+info.setLife(50)
 // Toggle "On"
 arrowtoggle = 1
 // All under this comment is Sprites.
-lparrow = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . 1 . . . . . . . . . . . 
-    . . . 1 . . . . . . . . . . . . 
-    . . 1 . . . . . . . . . . . . . 
-    . 1 . . . . . . . . . . . . . . 
-    1 1 1 1 1 1 1 1 . . . . . . . . 
-    . 1 . . . . . . . . . . . . . . 
-    . . 1 . . . . . . . . . . . . . 
-    . . . 1 . . . . . . . . . . . . 
-    . . . . 1 . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Misc)
+lparrow = sprites.create(assets.image`larrowint`, SpriteKind.Misc)
 rparrow = sprites.create(assets.image`myImage0`, SpriteKind.Misc)
-let chicken1 = sprites.create(img`
-    . . 2 2 b b b b b . . . . . . . 
-    . 2 b 4 4 4 4 4 4 b . . . . . . 
-    2 2 4 4 4 4 d d 4 4 b . . . . . 
-    2 b 4 4 4 4 4 4 d 4 b . . . . . 
-    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
-    2 2 b 4 4 4 4 4 4 4 b e . . . . 
-    . 2 b b b 4 4 4 b b b e . . . . 
-    . . e b b b b b b b e e . . . . 
-    . . . e e b 4 4 b e e e b . . . 
-    . . . . . e e e e e e b d b b . 
-    . . . . . . . . . . . b 1 1 1 b 
-    . . . . . . . . . . . c 1 d d b 
-    . . . . . . . . . . . c 1 b c . 
-    . . . . . . . . . . . . c c . . 
-    `, SpriteKind.Projectile)
-let pizza1 = sprites.create(img`
-    . . . . . . b b b b . . . . . . 
-    . . . . . . b 4 4 4 b . . . . . 
-    . . . . . . b b 4 4 4 b . . . . 
-    . . . . . b 4 b b b 4 4 b . . . 
-    . . . . b d 5 5 5 4 b 4 4 b . . 
-    . . . . b 3 2 3 5 5 4 e 4 4 b . 
-    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
-    . . . b 5 3 2 3 5 5 5 5 e e e e 
-    . . b d 7 5 5 5 3 2 3 5 5 e e e 
-    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
-    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
-    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
-    b d 3 2 d 5 5 5 d d d 4 4 . . . 
-    b 5 5 5 5 d d 4 4 4 4 . . . . . 
-    4 d d d 4 4 4 . . . . . . . . . 
-    4 4 4 4 . . . . . . . . . . . . 
-    `, SpriteKind.Projectile)
-let steak1 = sprites.create(img`
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . 2 2 3 3 3 3 2 e . . . . 
-    . . . 2 3 d 1 1 d d 3 2 e . . . 
-    . . 2 3 1 d 3 3 3 d d 3 e . . . 
-    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
-    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
-    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
-    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
-    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
-    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
-    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
-    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
-    e 3 d 3 3 1 d d 3 d 1 b b e e . 
-    . e 3 1 1 d d 1 1 1 b b e e e . 
-    . . e 3 3 3 3 3 3 b e e e e . . 
-    . . . e e e e e e e e e e . . . 
-    `, SpriteKind.Projectile)
-let cherrys1 = sprites.create(img`
-    . . . . . . . . . . . 6 6 6 6 6 
-    . . . . . . . . . 6 6 7 7 7 7 8 
-    . . . . . . 8 8 8 7 7 8 8 6 8 8 
-    . . e e e e c 6 6 8 8 . 8 7 8 . 
-    . e 2 5 4 2 e c 8 . . . 6 7 8 . 
-    e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
-    e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
-    e 2 e e 2 2 2 2 e e e e c 6 8 . 
-    c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
-    . c 2 e e e 2 e 2 4 2 2 2 2 c . 
-    . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
-    . . . e c c e c 2 2 2 2 2 2 2 e 
-    . . . . . . . c 2 e e 2 2 e 2 c 
-    . . . . . . . c e e e e e e 2 c 
-    . . . . . . . . c e 2 2 2 2 c . 
-    . . . . . . . . . c c c c c . . 
-    `, SpriteKind.Projectile)
-let apple1 = sprites.create(img`
-    . . . . . . . e c 7 . . . . . . 
-    . . . . e e e c 7 7 e e . . . . 
-    . . c e e e e c 7 e 2 2 e e . . 
-    . c e e e e e c 6 e e 2 2 2 e . 
-    . c e e e 2 e c c 2 4 5 4 2 e . 
-    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
-    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
-    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
-    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
-    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
-    . . . 2 2 e e 4 4 4 2 e e . . . 
-    . . . . . 2 2 e e e e . . . . . 
-    `, SpriteKind.Projectile)
-let strawberry1 = sprites.create(img`
-    . . . . . . . 6 . . . . . . . . 
-    . . . . . . 8 6 6 . . . 6 8 . . 
-    . . . e e e 8 8 6 6 . 6 7 8 . . 
-    . . e 2 2 2 2 e 8 6 6 7 6 . . . 
-    . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
-    . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
-    e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
-    e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
-    e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
-    e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
-    e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
-    e 2 2 2 2 2 2 2 4 e 2 e e c . . 
-    e e 2 e 2 2 4 2 2 e e e c . . . 
-    e e e e 2 e 2 2 e e e c . . . . 
-    e e e 2 e e c e c c c . . . . . 
-    . c c c c c c c . . . . . . . . 
-    `, SpriteKind.Projectile)
+chicken1 = sprites.create(assets.image`chicken🍗`, SpriteKind.Projectile)
+pizza1 = sprites.create(assets.image`pizza`, SpriteKind.Projectile)
+steak1 = sprites.create(assets.image`Steak`, SpriteKind.Projectile)
+cherrys1 = sprites.create(assets.image`cherry`, SpriteKind.Projectile)
+apple1 = sprites.create(assets.image`apple`, SpriteKind.Projectile)
+strawberry1 = sprites.create(assets.image`strawberry`, SpriteKind.Projectile)
+lifepotion10 = sprites.create(assets.image`myImage4`, SpriteKind.Misc)
+_2xspeedpotion = sprites.create(assets.image`potion0`, SpriteKind.Player)
 ball1.setStayInScreen(true)
 chicken1.setPosition(randint(0, 200), 0)
 pizza1.setPosition(randint(0, 200), 0)
@@ -186,13 +85,17 @@ steak1.setPosition(randint(0, 200), 0)
 cherrys1.setPosition(randint(0, 200), 0)
 apple1.setPosition(randint(0, 200), 0)
 strawberry1.setPosition(randint(0, 200), 0)
+_2xspeedpotion.setPosition(randint(0, 400), 0)
+lifepotion10.setPosition(randint(0, 300), 0)
+chicken1.setVelocity(15, 70)
+pizza1.setVelocity(-15, 100)
+steak1.setVelocity(15, 50)
+cherrys1.setVelocity(30, 30)
+apple1.setVelocity(50, 50)
+strawberry1.setVelocity(0, 150)
+lifepotion10.setVelocity(0, 100)
+_2xspeedpotion.setVelocity(0, 75)
 forever(function () {
-    chicken1.setVelocity(15, 70)
-    pizza1.setVelocity(-15, 100)
-    steak1.setVelocity(15, 50)
-    cherrys1.setVelocity(30, 30)
-    apple1.setVelocity(50, 50)
-    strawberry1.setVelocity(0, 150)
     pause(2500)
     info.changeScoreBy(1)
     chicken1.setPosition(randint(0, 200), 0)
@@ -201,6 +104,8 @@ forever(function () {
     cherrys1.setPosition(randint(0, 200), 0)
     apple1.setPosition(randint(0, 200), 0)
     strawberry1.setPosition(randint(0, 200), 0)
+    lifepotion10.setPosition(randint(0, 300), 0)
+    _2xspeedpotion.setPosition(randint(0, 400), 0)
 })
 forever(function () {
     lparrow.setPosition(10, 60)
@@ -236,4 +141,5 @@ forever(function () {
         info.changeLifeBy(-1)
         game.setGameOverMessage(false, "Strawberry hurt you")
     }
+    Potions()
 })
